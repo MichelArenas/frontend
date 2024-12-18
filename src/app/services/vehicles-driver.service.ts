4b089f8f6d752ca5e2de1db1c53288c097dab069
MichelArenas <michel.arenas28572@ucaldas.edu.co>
2024-12-18 11:59:15 -0500

    correciones driver-vehicles

--- src/app/services/vehicles-driver.service.ts
+++ src/app/services/vehicles-driver.service.ts
@@ -10,58 +10,19 @@ import { VehiclesDriver } from '../models/vehicles-driver.model';
 export class VehiclesDriverService {
 
   constructor(private http:HttpClient) { }
-  //observable es una clase que permite manejar eventos asincronos
-  list(): Observable<VehiclesDriver[]> {
-    return this.http.get<VehiclesDriver[]>(
-      `${environment.url_ms_logic}/VehiclesDriver`
-    );
+  list(): Observable<VehiclesDriver[]> { 
+    return this.http.get<VehiclesDriver[]>(`${environment.url_ms_logic}/VehiclesDriver`); 
   }
-
-  view(id: number): Observable<VehiclesDriver> {
-    //listar un solo teatro
-
-    return this.http.get<VehiclesDriver>(
-      `${environment.url_ms_logic}/VehiclesDriver/${id}`
-    );
+  delete(id:number) {
+    return this.http.delete<VehiclesDriver>(`${environment.url_ms_logic}/VehiclesDriver/${id}`);
   }
-
-  delete(id: number) {
-    return this.http.delete<VehiclesDriver>(
-      `${environment.url_ms_logic}/VehiclesDriver/${id}`
-    );
+  view(id:number): Observable<VehiclesDriver> {
+    return this.http.get<VehiclesDriver>(`${environment.url_ms_logic}/VehiclesDriver/${id}`);
   }
-
-  create(VehicleDriver: VehiclesDriver): Observable<VehiclesDriver> {
-    return this.http.post<VehiclesDriver>(
-      `${environment.url_ms_logic}/VehiclesDriver`,
-      VehicleDriver
-    );
+  create(newVehiclesDriver: VehiclesDriver): Observable<VehiclesDriver> {
+    return this.http.post<VehiclesDriver>(`${environment.url_ms_logic}/VehiclesDriver`,newVehiclesDriver);
   }
-  //en la proxima clase colocamos el interceptor
-
-  update(VehicleDriver: VehiclesDriver): Observable<VehiclesDriver> {
-    return this.http.put<VehiclesDriver>(
-      `${environment.url_ms_logic}/VehiclesDriver/${VehicleDriver.id}`,
-      VehicleDriver
-    );
+  update(theVehiclesDriver: VehiclesDriver): Observable<VehiclesDriver> {
+    return this.http.put<VehiclesDriver>(`${environment.url_ms_logic}/VehiclesDriver/${theVehiclesDriver.id}`,theVehiclesDriver);
   }
-  listByDriver(driver_id: number): Observable<VehiclesDriver[]> {
-           return this.http.get<VehiclesDriver[]>(`${
-             environment.url_ms_logic
-           }/VehiclesDriver?driver_id=${driver_id}`);
-           
-         }
-         createForDriver(driver_id: number, operation: VehiclesDriver): Observable<VehiclesDriver> {
-           return this.http.post<VehiclesDriver>(`${environment.url_ms_logic}/VehiclesDriver/driver/${driver_id}`, operation);
-         }
-       
-         listByVehicle(vehicle_id: number): Observable<VehiclesDriver[]> {
-           return this.http.get<VehiclesDriver[]>(`${
-             environment.url_ms_logic
-           }/VehiclesDriver?vehicle_id=${vehicle_id}`);
-           
-         }
-         createForVehicle(vehicle_id: number, vehicleOwners: VehiclesDriver): Observable<VehiclesDriver> {
-           return this.http.post<VehiclesDriver>(`${environment.url_ms_logic}/VehiclesDriver/vehicle/${vehicle_id}`, vehicleOwners);
-         }
 }
